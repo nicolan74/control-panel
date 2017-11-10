ef526dac5f9c92274e6498a9bc808aae261eb898
nicolan74 <nicola.lanzilotto@frontiere21.it>
2017-11-10 19:35:01 +0100

    integrata OneSignal Post Request

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { SearchProductService } from '../search-product.service';
 import { Product } from '../../models/product';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-products',
@@ -12,8 +13,11 @@ export class ProductsComponent implements OnInit {
   products: Product[];
   selectedProduct: Product;
 
+  // http: Http;
+
   constructor(
-    private searchProductService: SearchProductService
+    private searchProductService: SearchProductService,
+    private http: Http,
   ) { }
 
   ngOnInit() {
@@ -42,6 +46,27 @@ export class ProductsComponent implements OnInit {
     console.log('Selected Product - ID ', this.selectedProduct.id);
     console.log('Selected Product - NAME ', this.selectedProduct.name);
 
+  }
+
+  sendNotification() {
+    const headers = new Headers();
+    headers.append('Content-type', 'application/json');
+    headers.append('Authorization', 'Basic NzY1YjlhODUtNTc0Mi00ZmE3LWJjOGUtMzUxZTdjYmFhZTgw');
+    const options = new RequestOptions({ headers: headers });
+
+    const body = {
+      'app_id': 'ad8c202b-1223-494c-b7ea-3805a783cd33',
+      'contents': { 'en': 'Nikola Test' },
+      'included_segments': ['All'],
+      'data': { 'id': '623' }
+    };
+
+    console.log('POST REQUEST ', body);
+    this.http.post('https://onesignal.com/api/v1/notifications', JSON.stringify(body), options)
+      .map(res => res.json())
+      .subscribe(data => {
+        console.log('--->RESPONSE ', data);
+      });
   }
 
 }
